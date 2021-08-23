2b1f4c55f82ad21a9c086170ae6f0716c7305577
Smit6260 <47200932+Smit6260@users.noreply.github.com>
2021-08-23 10:23:21 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+  return "welcome"
+
+@app.route('/How are you')
+def hello():
+  return 'I am fine, what about you'
+
+if __name__ == "__main__":
+  app.run()
